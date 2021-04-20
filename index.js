ab5c2a060b1f452baa796e43d63c341ed178213a
MLO881 <74978616+MLO881@users.noreply.github.com>
2021-04-20 17:31:01 +0200

    Update index.js
    add ";"

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
-var Service, Characteristic
-const packageJson = require('./package.json')
-const request = require('request')
-const ip = require('ip')
-const http = require('http')
+var Service, Characteristic;
+const packageJson = require('./package.json');
+const request = require('request');
+const ip = require('ip');
+const http = require('http');
 
 module.exports = function (homebridge) {
-  Service = homebridge.hap.Service
-  Characteristic = homebridge.hap.Characteristic
-  homebridge.registerAccessory('homebridge-domoticz-thermostat', 'Thermostat', Thermostat)
-}
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory('homebridge-domoticz-thermostat', 'Thermostat', Thermostat);
+};
 
 function Thermostat (log, config) {
-  this.log = log
+  this.log = log;
 
-  this.name = config.name
-  this.apiroute = config.apiroute
-  this.pollInterval = config.pollInterval || 300
+  this.name = config.name;
+  this.apiroute = config.apiroute;
+  this.pollInterval = config.pollInterval || 300;
   
-  this.manufacturer = config.manufacturer || packageJson.author.name
-  this.serial = config.serial || this.apiroute
-  this.model = config.model || packageJson.name
-  this.firmware = config.firmware || packageJson.version
+  this.manufacturer = config.manufacturer || packageJson.author.name;
+  this.serial = config.serial || this.apiroute;
+  this.model = config.model || packageJson.name;
+  this.firmware = config.firmware || packageJson.version;
 
-  this.username = config.username || null
-  this.password = config.password || null
-  this.timeout = config.timeout || 3000
-  this.http_method = config.http_method || 'GET'
+  this.username = config.username || null;
+  this.password = config.password || null;
+  this.timeout = config.timeout || 3000;
+  this.http_method = config.http_method || 'GET';
 
-  this.heatOnly = config.heatOnly || false
+  this.heatOnly = config.heatOnly || false;
 
-  this.temperatureDisplayUnits = config.temperatureDisplayUnits || 0
-  this.maxTemp = config.maxTemp || 30
-  this.minTemp = config.minTemp || 15
-  this.minStep = config.minStep || 0.5
+  this.temperatureDisplayUnits = config.temperatureDisplayUnits || 0;
+  this.maxTemp = config.maxTemp || 30;
+  this.minTemp = config.minTemp || 15;
+  this.minStep = config.minStep || 0.5;
 
-  this.currentTemperatureIdx = config.currentTemperatureIdx
-  this.currentRelativeHumidity = config.currentRelativeHumidity || false
-  this.targetTemperatureIdx = config.targetTemperatureIdx
-  this.heatingCoolingStateIdx = config.heatingCoolingStateIdx || null
+  this.currentTemperatureIdx = config.currentTemperatureIdx;
+  this.currentRelativeHumidity = config.currentRelativeHumidity || false;
+  this.targetTemperatureIdx = config.targetTemperatureIdx;
+  this.heatingCoolingStateIdx = config.heatingCoolingStateIdx || null;
  
  
   if (this.username != null && this.password != null) {
     this.auth = {
       user: this.username,
       pass: this.password
-    }
-  }
+    };
+  };
 
-  this.service = new Service.Thermostat(this.name)
+  this.service = new Service.Thermostat(this.name);
 }
 
 
@@ -68,80 +68,80 @@ Thermostat.prototype = {
       auth: this.auth
     },
     function (error, response, body) {
-      callback(error, response, body)
-    })
+      callback(error, response, body);
+    });
   },
 
   _getStatus: function (callback) {
 	
   //get Temperature + humidity Data  
-	var url1 = this.apiroute + '/json.htm?type=devices&rid=' + this.currentTemperatureIdx
-	this.log.debug('Getting status URL1: %s', url1)
+	var url1 = this.apiroute + '/json.htm?type=devices&rid=' + this.currentTemperatureIdx;
+	this.log.debug('Getting status URL1: %s', url1);
 
 	this._httpRequest(url1, '', this.http_method, function (error, response, responseBody) {
 	  if (error) {
-		this.log.warn('Error getting status: %s', error.message)
-		this.service.getCharacteristic(Characteristic.CurrentTemperature).updateValue(new Error('Polling failed'))
-		callback(error)
+		this.log.warn('Error getting status: %s', error.message);
+		this.service.getCharacteristic(Characteristic.CurrentTemperature).updateValue(new Error('Polling failed'));
+		callback(error);
 	  } else {
-		this.log.debug('Device response: %s', responseBody)
-		var json = JSON.parse(responseBody)
-		this.service.getCharacteristic(Characteristic.CurrentTemperature).updateValue(json.result[0].Temp)
-		this.log.debug('Updated CurrentTemperature to: %s', json.result[0].Temp)
+		this.log.debug('Device response: %s', responseBody);
+		var json = JSON.parse(responseBody);
+		this.service.getCharacteristic(Characteristic.CurrentTemperature).updateValue(json.result[0].Temp);
+		this.log.debug('Updated CurrentTemperature to: %s', json.result[0].Temp);
 		if (this.currentRelativeHumidity) {
-			  this.service.getCharacteristic(Characteristic.CurrentRelativeHumidity).updateValue(json.result[0].Humidity)
-			  this.log.debug('Updated CurrentRelativeHumidity to: %s', json.result[0].Humidity)
+			  this.service.getCharacteristic(Characteristic.CurrentRelativeHumidity).updateValue(json.result[0].Humidity);
+			  this.log.debug('Updated CurrentRelativeHumidity to: %s', json.result[0].Humidity);
 		}
-		callback()
+		callback();
 	  }
-	}.bind(this))
+	}.bind(this));
     
     
   //get Target temperature
-	var url2 = this.apiroute + '/json.htm?type=devices&rid=' + this.targetTemperatureIdx
-	this.log.debug('Getting status URL2: %s', url2)
+	var url2 = this.apiroute + '/json.htm?type=devices&rid=' + this.targetTemperatureIdx;
+	this.log.debug('Getting status URL2: %s', url2);
 
 	this._httpRequest(url2, '', this.http_method, function (error, response, responseBody) {
 	  if (error) {
-      this.log.warn('Error getting status: %s', error.message)
-      this.service.getCharacteristic(Characteristic.TargetTemperature).updateValue(new Error('Polling failed'))
-      callback(error)
+      this.log.warn('Error getting status: %s', error.message);
+      this.service.getCharacteristic(Characteristic.TargetTemperature).updateValue(new Error('Polling failed'));
+      callback(error);
 	  } else {
-      this.log.debug('Device response: %s', responseBody)
-      var json = JSON.parse(responseBody)
-      this.service.getCharacteristic(Characteristic.TargetTemperature).updateValue(json.result[0].SetPoint)
-      this.log.debug('Updated TargetTemperature to: %s', json.result[0].SetPoint)
-      callback()
+      this.log.debug('Device response: %s', responseBody);
+      var json = JSON.parse(responseBody);
+      this.service.getCharacteristic(Characteristic.TargetTemperature).updateValue(json.result[0].SetPoint);
+      this.log.debug('Updated TargetTemperature to: %s', json.result[0].SetPoint);
+      callback();
 	  }
-	}.bind(this))	  
+	}.bind(this));
 	  
 	
 	//get current TargetHeatingCoolingState
-	this.log.debug('heatingCoolingStateIdx: %s', this.heatingCoolingStateIdx)
+	this.log.debug('heatingCoolingStateIdx: %s', this.heatingCoolingStateIdx);
 	if (this.heatingCoolingStateIdx != null) {
-		var url3 = this.apiroute + '/json.htm?type=devices&rid=' + this.heatingCoolingStateIdx
-		this.log.debug('Getting status URL3: %s', url3)
+		var url3 = this.apiroute + '/json.htm?type=devices&rid=' + this.heatingCoolingStateIdx;
+		this.log.debug('Getting status URL3: %s', url3);
 
 		this._httpRequest(url3, '', this.http_method, function (error, response, responseBody) {
 		  if (error) {
-			this.log.warn('Error getting status: %s', error.message)
-			this.service.getCharacteristic(Characteristic.TargetHeatingCoolingState).updateValue(new Error('Polling failed'))
-			callback(error)
+			this.log.warn('Error getting status: %s', error.message);
+			this.service.getCharacteristic(Characteristic.TargetHeatingCoolingState).updateValue(new Error('Polling failed'));
+			callback(error);
 		  } else {
-			this.log.debug('Device response: %s', responseBody)
-			var json = JSON.parse(responseBody)
+			this.log.debug('Device response: %s', responseBody);
+			var json = JSON.parse(responseBody);
 			if ( json.result[0].hasOwnProperty('Level') ) {
-				var Level = json.result[0].Level/10 
-				this.service.getCharacteristic(Characteristic.TargetHeatingCoolingState).updateValue(Level)
+				var Level = json.result[0].Level/10 ;
+				this.service.getCharacteristic(Characteristic.TargetHeatingCoolingState).updateValue(Level);
 			} else if ( json.result[0].hasOwnProperty('Mode') ) {
-				this.service.getCharacteristic(Characteristic.TargetHeatingCoolingState).updateValue(json.result[0].Mode)
-				this.log.debug('Updated TargetHeatingCoolingState to: %s', json.result[0].Mode)
+				this.service.getCharacteristic(Characteristic.TargetHeatingCoolingState).updateValue(json.result[0].Mode);
+				this.log.debug('Updated TargetHeatingCoolingState to: %s', json.result[0].Mode);
 			} else {
-				this.log.debug('Updated TargetHeatingCoolingState to: %s', 'error no hasOwnProperty')
+				this.log.debug('Updated TargetHeatingCoolingState to: %s', 'error no hasOwnProperty');
 			}
-			callback()
+			callback();
 		  }
-		}.bind(this))
+		}.bind(this));
 	} 
   },
   
@@ -149,76 +149,76 @@ Thermostat.prototype = {
   _httpHandler: function (characteristic, value) {
     switch (characteristic) {
       case 'targetHeatingCoolingState':
-        this.service.getCharacteristic(Characteristic.TargetHeatingCoolingState).updateValue(value)
-        this.log('Updated %s to: %s', characteristic, value)
-        break
+        this.service.getCharacteristic(Characteristic.TargetHeatingCoolingState).updateValue(value);
+        this.log('Updated %s to: %s', characteristic, value);
+        break;
       case 'targetTemperature':
-        this.service.getCharacteristic(Characteristic.TargetTemperature).updateValue(value)
-        this.log('Updated %s to: %s', characteristic, value)
-        break
+        this.service.getCharacteristic(Characteristic.TargetTemperature).updateValue(value);
+        this.log('Updated %s to: %s', characteristic, value);
+        break;
       default:
-        this.log.warn('Unknown characteristic "%s" with value "%s"', characteristic, value)
+        this.log.warn('Unknown characteristic "%s" with value "%s"', characteristic, value);
     }
   },
 
 
   setTargetHeatingCoolingState: function (value, callback) {
 	if (this.heatingCoolingStateIdx != null) {
-	    var nvalue= value 
-	    var svalue= value * 10
-	    var url = this.apiroute + '/json.htm?type=command&param=udevice&idx=' + this.heatingCoolingStateIdx + '&nvalue=' + nvalue + ' &svalue=' + svalue
-	    this.log.debug('Setting targetHeatingCoolingState: %s', url)
+	    var nvalue= value ;
+	    var svalue= value * 10;
+	    var url = this.apiroute + '/json.htm?type=command&param=udevice&idx=' + this.heatingCoolingStateIdx + '&nvalue=' + nvalue + ' &svalue=' + svalue;
+	    this.log.debug('Setting targetHeatingCoolingState: %s', url);
 
 	    this._httpRequest(url, '', this.http_method, function (error, response, responseBody) {
 	      if (error) {
-		this.log.warn('Error setting targetHeatingCoolingState: %s', error.message)
+		this.log.warn('Error setting targetHeatingCoolingState: %s', error.message);
 		callback(error)
 	      } else {
-		this.log('Set targetHeatingCoolingState to: %s', value)
-		this.service.getCharacteristic(Characteristic.CurrentHeatingCoolingState).updateValue(value)
-		callback()
+		this.log('Set targetHeatingCoolingState to: %s', value);
+		this.service.getCharacteristic(Characteristic.CurrentHeatingCoolingState).updateValue(value);
+		callback();
 	      }
-	    }.bind(this))
+	    }.bind(this));
   	}
   },
 
   setTargetTemperature: function (value, callback) {
-    value = value.toFixed(1)
-    var url = this.apiroute + '/json.htm?type=command&param=setsetpoint&idx=' + this.targetTemperatureIdx + '&setpoint=' + value
-    this.log.debug('Setting targetTemperature: %s', url)
+    value = value.toFixed(1);
+    var url = this.apiroute + '/json.htm?type=command&param=setsetpoint&idx=' + this.targetTemperatureIdx + '&setpoint=' + value;
+    this.log.debug('Setting targetTemperature: %s', url);
 
     this._httpRequest(url, '', this.http_method, function (error, response, responseBody) {
       if (error) {
-        this.log.warn('Error setting targetTemperature: %s', error.message)
-        callback(error)
+        this.log.warn('Error setting targetTemperature: %s', error.message);
+        callback(error);
       } else {
-        this.log('Set targetTemperature to: %s', value)
-        callback()
+        this.log('Set targetTemperature to: %s', value);
+        callback();
       }
-    }.bind(this))
+    }.bind(this));
   },
 
 
   getServices: function () {
-    this.informationService = new Service.AccessoryInformation()
+    this.informationService = new Service.AccessoryInformation();
     this.informationService
       .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
       .setCharacteristic(Characteristic.Model, this.model)
       .setCharacteristic(Characteristic.SerialNumber, this.serial)
-      .setCharacteristic(Characteristic.FirmwareRevision, this.firmware)
+      .setCharacteristic(Characteristic.FirmwareRevision, this.firmware);
 
-    this.service.getCharacteristic(Characteristic.TemperatureDisplayUnits).updateValue(this.temperatureDisplayUnits)
+    this.service.getCharacteristic(Characteristic.TemperatureDisplayUnits).updateValue(this.temperatureDisplayUnits);
 
     this.service
       .getCharacteristic(Characteristic.TargetHeatingCoolingState)
-      .on('set', this.setTargetHeatingCoolingState.bind(this))
+      .on('set', this.setTargetHeatingCoolingState.bind(this));
 
     if (this.heatOnly) {
       this.service.getCharacteristic(Characteristic.TargetHeatingCoolingState)
         .setProps({
           maxValue: Characteristic.TargetHeatingCoolingState.HEAT
-        })
-    }
+        });
+    };
 
     this.service
       .getCharacteristic(Characteristic.TargetTemperature)
@@ -227,15 +227,15 @@ Thermostat.prototype = {
         minValue: this.minTemp,
         maxValue: this.maxTemp,
         minStep: this.minStep
-      })
+      });
 
 
-    this._getStatus(function () {})
+    this._getStatus(function () {});
 
     setInterval(function () {
-      this._getStatus(function () {})
-    }.bind(this), this.pollInterval * 1000)
+      this._getStatus(function () {});
+    }.bind(this), this.pollInterval * 1000);
 
-    return [this.informationService, this.service]
+    return [this.informationService, this.service];
   }
 }
